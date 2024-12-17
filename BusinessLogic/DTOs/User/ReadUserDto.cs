2091f32b2ba1f0db946c9ae11c39b3e655c9a6cc
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2024-12-17 09:50:31 +0700

    fix swagger ui

--- BusinessLogic/DTOs/User/ReadUserDto.cs
+++ BusinessLogic/DTOs/User/ReadUserDto.cs
@@ -6,8 +6,6 @@
         public string? FullName { get; set; }
         public string? Username { get; set; }
         public string? Status { get; set; } // "Active" or "Banned"
-        public bool IsExternal { get; set; }
-        public string? ExternalProvider { get; set; }
         public List<string>? Roles { get; set; }
     }
 }
